5b3d952dcfeb50a5a9efc166992dc7f3df09ec16
Fedor <136792498+Fedor294@users.noreply.github.com>
2024-01-15 21:23:07 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,14 @@
+# import os
+#
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+# valid_email = os.getenv('valid_email')
+# valid_password = os.getenv('valid_password')
+
+valid_email = 'fedor161@mail.ru'
+valid_password = '123456'
+
+invalid_email = 'fedr116@mail.ru'
+invalid_password = '!@#приш'
